9cd31be711521659f87d73c19569be5a720df9dc
Subramanya K S <subramanyaks2205@gmail.com>
2022-05-06 17:02:44 +0530

    updated Bubble Sort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        in.close();
+        bubbleSort(arr);
+        for(int j=0;j<n;j++){
+           System.out.print(arr[j]);
+        }
+
+    }
+    public static void bubbleSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+}
