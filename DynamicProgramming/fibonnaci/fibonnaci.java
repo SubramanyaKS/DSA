041df9a401d5f8a89fe47b585c290060bb47c72a
Subramanya K S <subramanyaks2205@gmail.com>
2022-07-08 21:59:43 +0530

    added fib memo

---
+++ DynamicProgramming/fibonnaci/fibonnaci.java
@@ -0,0 +1,27 @@
+/**
+ * Fibonnaci Series ussing memorization Technique.
+ */
+/** 
+ * TC o(N) SC O(N)+O(N)
+ */
+import java.util.Arrays;
+import java.util.*;
+public class fibonnaci{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.close();
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(fib(n,dp));
+    }
+    public static int fib(int n,int[] dp){
+        if(n==0||n==1){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=fib(n-1,dp)+fib(n-2,dp);
+    }
+}
