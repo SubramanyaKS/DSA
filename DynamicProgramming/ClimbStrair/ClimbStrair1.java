850bc6425ad63df45020cdfbdba853dde4a8afd4
Subramanya K S <subramanyaks2205@gmail.com>
2022-07-09 10:29:48 +0530

    updated climbstrair tabulation

---
+++ DynamicProgramming/ClimbStrair/ClimbStrair1.java
@@ -0,0 +1,26 @@
+/**
+ * Climb Strair using tabulation method
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ClimbStrair1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        in.close();
+        int dp[]=new int [n+2];
+        Arrays.fill(dp,0);
+        System.out.println(climb(n,dp));
+
+    }
+    public static int climb(int n,int[] dp){
+        dp[n+1]=0;
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            dp[i]=dp[i+1]+dp[i+2];
+        }
+        return dp[0];
+
+    }
+}
