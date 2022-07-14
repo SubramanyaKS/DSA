66662e617b6903365ad1a2882efe49baef24a96c
Subramanya K S <subramanyaks2205@gmail.com>
2022-07-14 19:37:08 +0530

    added frog Jump tab

---
+++ DynamicProgramming/FrogJump/FrogJump1.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class FrogJump1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n=in.nextInt();
+        int height[]=new int[n];
+        for(int i=0;i<n;i++){
+            height[i]=in.nextInt();
+        }
+        in.close();
+        int dp[] = new int[n];
+        Arrays.fill(dp, -1);
+        frog(n,height,dp);
+
+
+    }
+    public static int frog(int n,int[] height,int[] dp){
+        dp[n]=100000;
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            int one = Math.abs(height[i+1]-height[i]+dp[i+1]);
+            int two = Math.abs(height[i+2]-height[i]+dp[i+2]);
+            dp[i]=Math.min(one,two);
+
+        }
+        return dp[0];
+    }
+}
