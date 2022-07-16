35a41d426bff63eacbb3e002b335de308a08d015
Subramanya K S <subramanyaks2205@gmail.com>
2022-07-16 22:30:33 +0530

    update house robber

--- DynamicProgramming/HouseRobber/HouseRobber.java
+++ DynamicProgramming/HouseRobber/HouseRobber.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class HouseRobber {
 
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+        }
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        house(0,n-1,arr,dp);
+
+    }
+    public static int house(int i,int v,int[] arr, int [] dp){
+
     }
 }
