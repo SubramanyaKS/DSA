e9a5bd2aa76b9b45e01b4d1ed03a1fca59ee04ba
Subramanya K S <subramanyaks2205@gmail.com>
2022-07-17 07:01:21 +0530

    updated house robber memorization

--- DynamicProgramming/HouseRobber/HouseRobber.java
+++ DynamicProgramming/HouseRobber/HouseRobber.java
@@ -1,3 +1,8 @@
+/**
+ * House Robber using Memorization 
+ * Maximum sum of Non adjacent element
+ * 
+ */
 import java.util.Arrays;
 import java.util.Scanner;
 public class HouseRobber {
@@ -9,12 +14,25 @@ public class HouseRobber {
         for(int i=0;i<n;i++){
             arr[i]=in.nextInt();
         }
+        in.close();
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        house(0,n-1,arr,dp);
+       System.out.println(house(0,n-1,arr,dp)) ;
 
     }
     public static int house(int i,int v,int[] arr, int [] dp){
-
+        if(i==v){
+            return arr[i];
+        }
+        if(i>v){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int r=arr[i]+house(i+2,v,arr,dp);
+        int d=house(i+1, v, arr, dp);
+        dp[i]=Math.max(r,d);
+        return dp[i];
     }
 }
