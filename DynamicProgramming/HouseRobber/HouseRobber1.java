b05260010ebf1478eebe79fc86a0da343076dc41
Subramanya K S <subramanyaks2205@gmail.com>
2022-07-17 07:01:41 +0530

    updated house robber tabulation

---
+++ DynamicProgramming/HouseRobber/HouseRobber1.java
@@ -0,0 +1,37 @@
+/**
+ * House Robber using tabulation
+ * Maximum sum of Non adjacent element
+ * 
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HouseRobber1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=in.nextInt();
+
+        }
+        in.close();
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,0);
+        System.out.println(rob(arr,dp));
+    }
+    public static int rob(int arr[],int dp[]){
+        int n=arr.length;
+        dp[n-1]=arr[n-1];
+        dp[n]=0;
+        for(int i=n-2;i>=0;i--){
+            int r=arr[i]+dp[i+2];
+            int d=dp[i+1];
+            dp[i]=Math.max(r,d);
+        }
+        return dp[0];
+
+    }
+
+    
+}
