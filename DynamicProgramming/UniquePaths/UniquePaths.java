4b414ba1415c5c8c9d7f46e2f71d2769f2884b40
Subramanya K S <subramanyaks2205@gmail.com>
2022-08-06 12:59:52 +0530

    updated unique path

--- DynamicProgramming/UniquePaths/UniquePaths.java
+++ DynamicProgramming/UniquePaths/UniquePaths.java
@@ -10,15 +10,33 @@ public class UniquePaths {
         //column
         int n=in.nextInt();
         in.close();
-        int dp[][]= new int [m][n];
-        for(int i=0;i<n;i++){
-            dp[m][i]=-1;
+        int dp[][]= new int [m+1][n+1];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
         }
-        paths(0,0,m-1,n-1,dp);
+        System.out.println(paths(0,0,m-1,n-1,dp));
 
     }
     public static int paths(int cr,int cc,int tr,int tc,int[][] dp){
-        if()
+        /**
+         * cc= current column
+         * cr= current row
+         * tc= target column
+         * tr = target row
+         */
+        if(cr==tr && cc==tc){
+            return 1;
+        }
+        if(cr>tr || cc>tc){
+            return 0;
+        }
+        if(dp[cr][cc]!=-1){
+            return dp[cr][cc];
+        }
+        dp[cr][cc]=paths(cr+1, cc, tr, tc, dp)+paths(cr, cc+1, tr, tc, dp);
+        return dp[cr][cc];
     }
     
 }
