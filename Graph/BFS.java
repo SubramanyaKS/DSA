9dc51963adfa865d07bde0868ae6f5fc37e75bf1
Subramanya K S <subramanyaks2205@gmail.com>
2022-11-28 07:22:49 +0530

    added BFS

---
+++ Graph/BFS.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class BFS {
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    // constructor
+    BFS(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new LinkedList<Integer>();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void BFSTraversal(int s) {
+        boolean visited[] = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<>();
+        visited[s] = true;
+        queue.add(s);
+        while (queue.size() != 0) {
+            s = queue.poll();
+            System.out.print(s + " ");
+
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        BFS b = new BFS(4);
+        b.addEdge(0, 1);
+        b.addEdge(0, 2);
+        b.addEdge(1, 2);
+        b.addEdge(2, 0);
+        b.addEdge(2, 3);
+        b.addEdge(3, 3);
+        b.BFSTraversal(2);
+    }
+
+}
