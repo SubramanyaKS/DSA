1d35011c09b475aeb4d9cd0d27e14d378f6bd3ef
Subramanya K S <subramanyaks2205@gmail.com>
2022-11-29 16:57:17 +0530

    updated MaxDepth

---
+++ Tree/MaxDepthBTree.java
@@ -0,0 +1,12 @@
+// Maximum Depth of Binary Tree 
+public class MaxDepthBTree {
+    public int maxDepth(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        int l= maxDepth(node.left);
+        int r= maxDepth(node.right);
+        return 1+Math.max(l, r);
+    }
+    
+}
