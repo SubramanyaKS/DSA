1b655ec535d44bd1288d524dc8b4601902046b85
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-12 20:11:19 +0530

    updated

---
+++ Graph/TopologicalSort.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+/**
+ * TopologicalSort
+ */
+public class TopologicalSort {
+    static int[] topoSort(int v, ArrayList<ArrayList<Integer>> adj){
+        boolean visted[] = new boolean[v];
+        Stack<Integer> st = new Stack<Integer>();
+        for(int i=0;i<v;i++){
+            if(!visted[i]){
+                dfs(adj,i,st,visted);
+            }
+        }
+        int ans[] = new int[v];
+        int i=0;
+        while(!st.isEmpty()){
+            ans[i++]= st.pop();
+        }
+        return ans;
+
+
+    }
+    static void dfs(ArrayList<ArrayList<Integer>> adj, int v, Stack<Integer> st, boolean visited[]){
+        visited[v]=true;
+        for(int n: adj.get(v)){
+            if(!visited[n]){
+                dfs(adj, n, st, visited);
+            }
+        }
+        st.push(v);
+
+    }
+
+    
+}
