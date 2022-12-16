d798e02583dfafe691f8d189d48b2ab54a5ddc04
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-16 07:19:24 +0530

    updated prim's algorithm

--- Graph/PrimsAlgorithm.java
+++ Graph/PrimsAlgorithm.java
@@ -1,16 +1,5 @@
 import java.util.*;
-class Pair implements Comparable<Pair>{
-    int w;
-    int v;
-    Pair(int v,int w){
-        this.w=w;
-        this.v=v;
-    }
-    public int compareTo(Pair that){
-        return this.w=that.w;
-    }
 
-}
 public class PrimsAlgorithm {
 
 
