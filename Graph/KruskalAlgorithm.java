1100502e798138a6c21c7b1bd44f19750c8c79f1
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-17 22:53:35 +0530

    updated kruskal

--- Graph/KruskalAlgorithm.java
+++ Graph/KruskalAlgorithm.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class KruskalAlgorithm {
     static int p[],rank[];
@@ -21,11 +21,35 @@ public class KruskalAlgorithm {
 
     static int spanningTree(int v, ArrayList<ArrayList<ArrayList<Integer>>> adj){
         boolean added[][] = new boolean[v][v];
-        ArrayList<Edge> = new ArrayList<>();
+        ArrayList<Edge> edge = new ArrayList<>();
         for(int i=0;i<adj.size();i++){
             for (int j = 0; j < adj.get(i).size(); j++) {
+                ArrayList<Integer> cur= adj.get(i).get(j);
+                if(!added[i][cur.get(0)]){
+                    added[i][cur.get(0)]=true;
+                    added[cur.get(0)][i]=true;
+                    edge.add(new Edge(i,cur.get(0),cur.get(i)));
+                }
                 
             }
         }
+        p= new int[v];
+        for(int i=0;i<v;i++){
+            p[i]=i;
+        }
+        Collections.sort(edge);
+        int count=1;
+        int ans=0;
+        for(int i=0;count<v;i++){
+            Edge edges= edge.get(i);
+            int rx= find(edges.src);
+            int ry= find(edges.dest);
+            if(rx!=ry){
+                union(rx, ry);
+                count++;
+                ans+=edges.wt;
+            }
+        }
+        return ans;
     }
 }
