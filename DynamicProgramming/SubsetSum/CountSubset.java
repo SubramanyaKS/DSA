27e34b0e472197216bf96adb85928c5d29d2f4e6
Subramanya K S <subramanyaks2205@gmail.com>
2022-12-30 22:09:08 +0530

    updated count subset

---
+++ DynamicProgramming/SubsetSum/CountSubset.java
@@ -0,0 +1,32 @@
+package SubsetSum;
+
+//import java.util.*;
+
+public class CountSubset {
+    public static void main(String[] args) {
+        int n = 4;
+        int a[] = { 3, 3, 3, 3 };
+        int sum = 6;
+        System.out.println(countSubSet(a, n, sum));
+    }
+
+    public static int countSubSet(int a[], int n, int sum) {
+        int tab[][] = new int[n + 1][sum + 1];
+        tab[0][0] = 1;
+
+        for (int i = 1; i <= sum; i++)
+            tab[0][i] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= sum; j++) {
+                if (a[i - 1] > j)
+                    tab[i][j] = tab[i - 1][j];
+
+                else {
+                    tab[i][j] = tab[i - 1][j] + tab[i - 1][j - a[i - 1]];
+                }
+            }
+        }
+        return tab[n][sum];
+    }
+}
