cb56cdf641a43ad62d5417ed46e119ea8da3a4ae
Subramanya K S <subramanyaks2205@gmail.com>
2023-01-25 18:40:57 +0530

    updated with reset

--- Bit/SetkthBit.java
+++ Bit/SetkthBit.java
@@ -7,8 +7,12 @@ public class SetkthBit {
         in.close();
         SetkthBit b = new SetkthBit(); 
         b.setBit(n, k); 
+        b.resetBit(n,k);
     }
     public void setBit(long n, long k){
         System.out.println((1<<k )| n);
+    }
+    public void resetBit(long n, long k){
+        System.out.println((n & (~(1 << (k - 1)))));
     }
 }
