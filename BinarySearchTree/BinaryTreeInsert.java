e89f88b210574cf1a62a5000a89317c502e36eac
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-07 07:05:29 +0530

    updated BinarySearchTree insertion

---
+++ BinarySearchTree/BinaryTreeInsert.java
@@ -0,0 +1,16 @@
+public class BinaryTreeInsert {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root==null){
+            root = new TreeNode(val);
+            return root;
+        }
+        if(val<root.val){
+            root.left = insertIntoBST(root.left,val);
+        }
+        if(val>root.val){
+            root.right=insertIntoBST(root.right,val);
+        }
+        return root;
+    }
+    
+}
