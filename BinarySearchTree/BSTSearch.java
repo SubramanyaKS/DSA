7179688287b4a72cd2864ee1da00cb95ecedd874
Subramanya K S <subramanyaks2205@gmail.com>
2023-03-07 07:05:44 +0530

    updated BST search

---
+++ BinarySearchTree/BSTSearch.java
@@ -0,0 +1,11 @@
+public class BSTSearch {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null || root.val ==val){
+            return root;
+        }
+        if(root.val < val){
+            return searchBST(root.right,val);
+        }
+        return searchBST(root.left,val);
+    }
+}
